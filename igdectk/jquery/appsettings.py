9865661a4e3a290498f9823c817c0c174ab80f82
Frederic Scherma <frederic.scherma@clermont.inra.fr>
2016-02-01 17:23:56 +0100

    ADD: select2 3.5.4 and bootstrap extension ADD: bootstrap glyph python enum

--- igdectk/jquery/appsettings.py
+++ igdectk/jquery/appsettings.py
@@ -10,7 +10,7 @@ APP_VERBOSE_NAME = "igdectk.jquery"
 
 APP_SETTINGS_MODEL = None
 
-APP_VERSION = (1, 0, 2)
+APP_VERSION = (1, 0, 3)
 
 DEFAULT_PROPERTIES = {
     'jquery': {'js': 'jquery.min.js', 'css': '', 'default_version': '2.1.4', 'versions': ('2.1.4',)},
@@ -19,7 +19,10 @@ DEFAULT_PROPERTIES = {
         'js': 'jquery.fancytree.min.js', 'css': 'skin-%(theme)s/ui.fancytree.min.css', 'default_version': '2.12.0', 'versions': ('2.12.0', '2.13.0'), 'default_theme': 'bootstrap', 'themes': ('bootstrap',),
         '.glyph': {'js': 'jquery.fancytree.glyph.min.js', 'css': '', 'default_version': '2.12.0', 'versions': ('2.12.0', '2.13.0'), 'default_theme': ''}
     },
-    'select2': {'js': 'select2.min.js', 'css': 'select2.css', 'default_version': '3.5.1', 'versions': ('3.5.1',)},
+    'select2': {
+        'js': 'select2.min.js', 'css': 'select2.css', 'default_version': '3.5.4', 'versions': ('3.5.1', '3.5.4'),
+        '.bootstrap': {'js': '', 'css': 'select2-bootstrap.css', 'default_version': '3.5.4', 'versions': ('3.5.1', '3.5.4')},
+    },
     'colpick': {'js': 'colpick.js', 'css': 'colpick.css', 'default_version': '1.0.0', 'versions': ('1.0.0',)},
     'numeric': {'js': 'jquery.numeric.js', 'css': '', 'default_version': '1.4.1', 'versions': ('1.4.1',)},
     'igdectk': {'js': 'igdectk.js', 'css': '', 'default_version': '1.0.0', 'versions': ('1.0.0',),
